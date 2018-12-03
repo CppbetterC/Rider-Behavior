4ee5fd9d6479ea7ff06ae5a0de322c0e818cc741
CPPBETTERC\yuren <highlight011385@gmail.com>
2018-12-03 21:05:24 +0800

    Update the Method3

--- Main2.py
+++ Main2.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 import keras
 from keras import optimizers
@@ -113,19 +114,18 @@ def train_local_fnn(nn, algorithm):
     print('<----------------------------------------------->')
 
     # Choose the best FNN to Plot error trend
-    rel_path = './Experiment/Method3/Graph/Best_FNN_'+str(nn)+'_error_trend.png'
-    abs_path = os.path.join(os.path.dirname(__file__), rel_path)
-    ErrorPlot.error_trend(
-        'Best_FNN_'+str(nn)+'_error_trend',
-         len(fnn_copy.error_list), fnn_copy.error_list, abs_path)
+    # rel_path = './Experiment/Method3/Graph/Best_FNN_'+str(nn)+'_error_trend.png'
+    # abs_path = os.path.join(os.path.dirname(__file__), rel_path)
+    # ErrorPlot.error_trend('Best_FNN_'+str(nn)+'_error_trend',
+    #                       len(fnn_copy.error_list), fnn_copy.error_list, abs_path)
 
     # Choose the best Accuracy to Plot
-    rel_path = './Experiment/Method3/Graph/Accuracy vs FNN'+str(nn)+'.png'
-    abs_path = os.path.join(os.path.dirname(__file__), rel_path)
-    AccuracyPlot.build_accuracy_plot(
-        'Accuracy vs FNN' +
-        str(nn), np.array([i for i in range(1, len(all_nn_accuracy) + 1, 1)]),
-        all_nn_accuracy, abs_path)
+    # rel_path = './Experiment/Method3/Graph/Accuracy vs FNN'+str(nn)+'.png'
+    # abs_path = os.path.join(os.path.dirname(__file__), rel_path)
+    # AccuracyPlot.build_accuracy_plot(
+    #    'Accuracy vs FNN' +
+    #    str(nn), np.array([i for i in range(1, len(all_nn_accuracy) + 1, 1)]),
+    #    all_nn_accuracy, abs_path)
 
     return fnn_copy, accuracy, matrix
 
@@ -140,12 +140,10 @@ def show_train_history(train_history, train, validation, file_name):
     plt.show()
 def train_keras_lnn(nn_category, org_data, org_label, algorithm):
     """Get the fnn output and input the lnn"""
-    X_train, X_test, y_train, y_test = train_test_split(
-        org_data, org_label, test_size=0.3, random_state=42)
     fnn_output = np.array([])
     for nn in nn_category:
         print('<---nn -> ', nn, '--->')
-        rel_path = '../Experiment/Method3/FNNModel/'+str(nn)+'.json'
+        rel_path = './Experiment/Method3/FNNModel/'+str(nn)+'.json'
         abs_path = os.path.join(os.path.dirname(__file__), rel_path)
         attribute = LoadData.load_fnn_weight(abs_path)
         mean = np.asarray(attribute['Mean'])
@@ -154,13 +152,17 @@ def train_keras_lnn(nn_category, org_data, org_label, algorithm):
         # Test the FNN
         fnn = FNN(
             fnn_input_size, fnn_membership_size, fnn_rule_size, fnn_output_size, mean, stddev, weight, fnn_lr, 1)
-        result = fnn.testing_model(X_train)
+        result = fnn.testing_model(org_data)
         fnn_output = np.append(fnn_output, result)
-    fnn_output = fnn_output.reshape(len(nn_category), -1)
+    fnn_output = fnn_output.reshape(len(nn_category), -1).T
+
+    fnn_label = np.array([value-1 for value in org_label])
+    X_train, X_test, y_train, y_test = train_test_split(fnn_output, fnn_label, test_size=0.3, random_state=42)
 
     # Construct the lnn
     y_trainOneHot = np_utils.to_categorical(y_train)
     y_testOneHot = np_utils.to_categorical(y_test)
+
     model = Sequential()
     model.add(Dense(units=6, input_dim=6))
     model.add(Dense(6, activation='tanh'))
@@ -170,8 +172,8 @@ def train_keras_lnn(nn_category, org_data, org_label, algorithm):
     model.summary()
 
     train_history = model.fit(x=X_train, y=y_trainOneHot, validation_split=0.2, epochs=30, batch_size=200, verbose=2)
-    show_train_history(train_history, 'mean_squared_error', 'val_mean_squared_error', 'mean_squared_error.png')
-    show_train_history(train_history, 'loss', 'val_loss', 'loss.png')
+    #show_train_history(train_history, 'mean_squared_error', 'val_mean_squared_error', 'mean_squared_error.png')
+    #show_train_history(train_history, 'loss', 'val_loss', 'loss.png')
 
     scores = model.evaluate(X_test, y_testOneHot)
     print('scores', scores)
@@ -182,13 +184,15 @@ def train_keras_lnn(nn_category, org_data, org_label, algorithm):
 
     prediction = model.predict_classes(X_test)
     cnf_matrix = confusion_matrix(y_test, prediction)
+    print('accuracy_score', accuracy_score(y_test, prediction))
+    print('cnf_matrix\n', cnf_matrix)
     rel_path = './Experiment/method3/Graph/cnf_lnn.png'
     abs_path = os.path.join(os.path.dirname(__file__), rel_path)
     ConfusionMatrix.plot_confusion_matrix(cnf_matrix, abs_path,
-                                          classes=list(set(y_test)), title='Final Model Confusion matrix')
+                                         classes=list(set(y_test)), title='Final Model Confusion matrix')
     # 模型存起來
-    path_name = 'FinalModel.h5'
-    model.save(path_name)
+    # path_name = 'FinalModel.h5'
+    # model.save(path_name)
 
 
 if __name__ == '__main__':
@@ -208,11 +212,11 @@ if __name__ == '__main__':
             fnn_accuracy.append(accuracy)
             fnn_matrix.append(matrix)
         
-        for i in range(len(fnn_matrix)):
-            rel_path = './Experiment/method3/Graph/cnf'+str(i)+'.png'
-            abs_path = os.path.join(os.path.dirname(__file__), rel_path)
-            ConfusionMatrix.plot_confusion_matrix(
-                fnn_matrix[i], abs_path, classes=[0,1], title='C'+str(i)+' Cnf')
+        #for i in range(len(fnn_matrix)):
+            #rel_path = './Experiment/method3/Graph/cnf'+str(i)+'.png'
+           # abs_path = os.path.join(os.path.dirname(__file__), rel_path)
+            #ConfusionMatrix.plot_confusion_matrix(
+               # fnn_matrix[i], abs_path, classes=[0,1], title='C'+str(i)+' Cnf')
 
 
         print('<---Part2, Keras Networks(LNN)--->')
